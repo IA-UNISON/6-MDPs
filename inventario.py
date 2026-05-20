6c7d44fbeee0ac04db83676ad1aaab544e2005c3
Alejandro Barreras <abarrerasgtz@gmail.com>
2026-05-19 18:17:26 -0700

    Se agrego el codigo faltante a inventario.py

--- inventario.py
+++ inventario.py
@@ -3,6 +3,8 @@ Para desarrollar el problema del inventario.
 
 """
 
+import math
+
 from MDPs import MDP, iteracion_valor
 
 class Inventario(MDP):
@@ -17,28 +19,39 @@ class Inventario(MDP):
     
     """    
     
-    def __init__(self, gama,lambda_, ...): #TODO: Agregar lo que se requiera
-        #TODO: Completar el constructor
-        self.gamma = #TODO: Agregar lo que se requiera
-        self.lambda_ = #TODO: Agregar lo que se requiera
-        self.estados = #TODO: Agregar lo que se requiera
-        #TODO: Agregar lo que se requiera
+    def __init__(self, gama,lambda_,capacidad, costo_orden, costo_almacenamiento):
+        self.gamma = gama
+        self.lambda_ = lambda_
+        self.capacidad = capacidad
+        self.costo_orden = costo_orden
+        self.costo_almacenamiento = costo_almacenamiento
+        self.estados = list(range(capacidad + 1))  
     
     def acciones_legales(self, s):
-        #TODO: Completar este método
-        pass
+        return list(range(0, self.capacidad - s + 1))
     
     def recompensa(self, s, a, s_):
-        #TODO: Completar este método
-        pass
+        return - (self.costo_orden * a + self.costo_almacenamiento * s_)
+    
+    def prob_poisson(self, k):
+        if k < 0:
+            return 0.0
+        return (math.exp(-self.lambda_) * self.lambda_**k) / math.factorial(k)
         
     def prob_transicion(self, s, a, s_):
-        #TODO: Completar este método
-        pass
+        inventario = s + a
+        
+        if s_ > 0:
+            demada = inventario - s_
+            return self.prob_poisson(demada)
+        else:
+            prob = 0.0
+            for demada in range(inventario + 1):
+                prob += self.prob_poisson(demada)
+            return prob
                 
     def es_terminal(self, s):
-        #TODO: Completar este método
-        pass
+        return False
 
 
 if __name__ == "__main__":
@@ -59,12 +72,24 @@ if __name__ == "__main__":
 Contesta las preguntas aquí mismo (has espacio entre las preguntas):
 
 1. ¿Cómo se comporta las transiciones y las ganancias para casos específicos de $s$ y $a$? 
+
+
 2. ¿Qué psa si hay mucho almacen? 
+
+
 3. ¿Que pasa si hay muy poco o estamos sin almacen? 
+
+
 4. ¿Existe un punto donde la ganancia sea máxima?  
+
 ---
+
 5. ¿Cómo se ve la política óptima? ¿Tiene sentido?
+
+
 6. ¿Como se comporta la función de valor de estado V(s)?
+
+
 7. ¿Cómo cambiaría la política si la variabilidad de la demanda (lambda) aumenta de 4 a 8?
 
 """
