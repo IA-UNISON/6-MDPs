7e190f4e373957aa5239882d902c1cd5161d2c0f
Juan PZ <a223201410@unison.mx>
2026-05-23 20:14:52 -0700

    Código del problema (#1)
    * wip: init y acciones_legales
    
    * feat: Funciones de Inventario
    
    * fix: acciones_legales y otras funciones
    
    * feat: Generalización

--- inventario.py
+++ inventario.py
@@ -7,45 +7,112 @@ from MDPs import MDP, iteracion_valor
 
 class Inventario(MDP):
     """
-    Clase que representa un MDP para el problema del camión mágico.
+    MDP para el problema del inventario.  
+    """
+    def __init__(self, 
+                 gama = 0.95, 
+                 lambda_= 4, 
+                 estados = (-10,20),
+                 k_max = 15, 
+                 precio_venta = 150, 
+                 costo_compra = 80, 
+                 costo_fijo_pedido = 40, 
+                 costo_almacenamiento = 5,
+                 costo_backlog = 15):
     
-    Si caminas, avanzas 1 con coso 1
-    Si usas el camion, con probabilidad rho avanzas el doble de donde estabas
-    y con probabilidad 1-rho te quedas en el mismo lugar. Todo con costo 2.
-    
-    El objetivo es llegar a la meta en el menor costo posible
-    
-    """    
-    
-    def __init__(self, gama,lambda_, ...): #TODO: Agregar lo que se requiera
-        #TODO: Completar el constructor
-        self.gamma = #TODO: Agregar lo que se requiera
-        self.lambda_ = #TODO: Agregar lo que se requiera
-        self.estados = #TODO: Agregar lo que se requiera
-        #TODO: Agregar lo que se requiera
+        # Parametros MDP
+        self.gama = gama
+        self.lambda_ = lambda_
+        self.k_max = k_max
+
+        # Ingresos
+        self.precio_venta = precio_venta
+        # Costos
+        self.costo_compra = costo_compra
+        self.costo_fijo_pedido = costo_fijo_pedido
+        self.costo_almacenamiento = costo_almacenamiento
+        self.costo_perdida = costo_backlog + (self.precio_venta - self.costo_compra)
+
+        self.estados = tuple([i for i in range(estados[0], estados[1]+1)])
     
     def acciones_legales(self, s):
-        #TODO: Completar este método
-        pass
+        rango = self.estados[-1] - s
+        return [i for i in range(rango + 1)]
     
-    def recompensa(self, s, a, s_):
-        #TODO: Completar este método
-        pass
-        
-    def prob_transicion(self, s, a, s_):
-        #TODO: Completar este método
-        pass
-                
-    def es_terminal(self, s):
-        #TODO: Completar este método
-        pass
+    def recompensa(self, s, a, s_=None):
+        from math import factorial, exp
 
+        def precio_venta(s, a):
+            suma_esperada = 0.0
+            
+            disponible = max(s + a, 0)
+            
+            for k in range(self.k_max + 1):
+                probabilidad = (exp(-self.lambda_) * (self.lambda_ ** k)) / factorial(k)
+                
+                vendido = min(disponible, k)
+            
+                suma_esperada += (self.precio_venta * vendido) * probabilidad
+                
+            return suma_esperada
+               
+        def costo_compra_mas_pedido(a):
+            costo = a * self.costo_compra
+            return costo + self.costo_fijo_pedido if a > 0 else costo
+        
+        def costo_almacenamiento(s,a):
+            suma_esperada = 0.0
+
+            for k in range(self.k_max + 1):
+                probabilidad = (exp(-self.lambda_) * (self.lambda_ ** k)) / factorial(k)
+                suma_esperada += max((s + a)-k, 0) * probabilidad
+                
+            return self.costo_almacenamiento * suma_esperada
+        
+        def costo_inv_negativo(s,a):
+            suma_esperada = 0.0
+
+            for k in range(self.k_max + 1):
+                probabilidad = (exp(-self.lambda_) * (self.lambda_ ** k)) / factorial(k)
+                suma_esperada += max(k - (s + a), 0) * probabilidad
+
+            return self.costo_perdida * suma_esperada
+        
+        return precio_venta(s,a) + (-costo_compra_mas_pedido(a)) + (-costo_almacenamiento(s,a)) + (-costo_inv_negativo(s,a))
+         
+    def prob_transicion(self, s, a, s_):
+        from math import factorial, exp
+        
+        D = s + a - s_
+
+        if D >= 0 and s_ > self.estados[0]:
+            probabilidad = (exp(-self.lambda_) * (self.lambda_ ** D)) / factorial(D)
+            return probabilidad
+            
+        if s_ > s + a:
+            return 0.0
+        
+        if s_ == self.estados[0]:
+            suma_esperada = 0.0
+
+            for k in range((s+a-self.estados[0]), self.k_max+1):
+                probabilidad = (exp(-self.lambda_) * (self.lambda_ ** k)) / factorial(k)
+                suma_esperada += probabilidad
+
+            return suma_esperada
+
+        return 0.0
+             
+    def es_terminal(self, s):
+        return False # No hay un estado terminal
 
 if __name__ == "__main__":
 
-    inventario = Inventario(0.9, 0.5, ...)  #TODO: Agregar lo que se requiera
+    inventario = Inventario(0.95, 4)
 
-    pi_star, V = iteracion_valor(inventario, ...) #TODO: Agregar lo que se requiera
+    #inventario = Inventario(0.95, 4, (-10, 10), 14, 140, 70, 50, 10, 20)
+
+    pi_star, V = iteracion_valor(inventario)
 
     print("-" * 60)
     print("Estado".center(20) + "Acción".center(20) + "Valor".center(20))
@@ -58,8 +125,8 @@ if __name__ == "__main__":
 """
 Contesta las preguntas aquí mismo (has espacio entre las preguntas):
 
-1. ¿Cómo se comporta las transiciones y las ganancias para casos específicos de $s$ y $a$? 
-2. ¿Qué psa si hay mucho almacen? 
+1. ¿Cómo se comportan las transiciones y las ganancias para casos específicos de $s$ y $a$? 
+2. ¿Qué pasa si hay mucho almacen? 
 3. ¿Que pasa si hay muy poco o estamos sin almacen? 
 4. ¿Existe un punto donde la ganancia sea máxima?  
 ---
