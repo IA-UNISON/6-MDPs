fd09fecf90b3881f6013c756750892f42737efa6
diegoo_ghz <diego.gallegos.hds@gmail.com>
2026-05-27 07:36:10 -0700

    analisis y preguntas

--- inventario.py
+++ inventario.py
@@ -23,9 +23,12 @@ class Inventario(MDP):
         return list(range(0, 21 - s))
     
     def recompensa(self, s, a, s_):
+        """
+        Le resto todos los diferentes costos a los ingresos.
+        """
         x = s + a
         d = x - s_
-        
+
         ventas = min(x, d) if x > 0 else 0
         unmet = max(d - x, 0)
 
@@ -98,16 +101,31 @@ if __name__ == "__main__":
 Contesta las preguntas aquí mismo (has espacio entre las preguntas):
 
 1. ¿Cómo se comporta las transiciones y las ganancias para casos específicos de $s$ y $a$? 
+    las transiciones dependen de cuanto sea la demanda aleatoria el siguiente dia. el inventario al dia siguiente es primero
+    la suma de s + a, a lo que luego se le resta la demanda para determinar s_. si la demanda es muy alta podemos tener inventario
+    negativo.
 
 2. ¿Qué psa si hay mucho almacen?
+    El costo del almacen se vuelve muy alto y la politica correcta en esos casos, como muestran los resultados, es no pedir mas unidades.
 
 3. ¿Que pasa si hay muy poco o estamos sin almacen?
+    Si hay muy poco o tenemos inventario negativo la politica correcta es pedir un mayor numero de unidades, ya que en la implementacion 
+    tambien estamos penalizando el backlog y la perdida de oprtunidades
 
 4. ¿Existe un punto donde la ganancia sea máxima?  
+    segun los resultados obtenidos es optimo mantenernos entre el rango de 6 a 9 unidades aproximadamente.
 
 ---
+
 5. ¿Cómo se ve la política óptima? ¿Tiene sentido?
+    Tiene sentido y es lo esperado (cuando hay bajo inventario se piden mas undidades, cuando hay mucho inventario se piden menos),
+    lo unico que no tiene sentido para mi es el salto brusco entre 5 y 6 en el que pasamos de pedir 4 unidades a 0. Tal vez algo 
+    hice mal en la implementacion pero el resto de la politica tiene sentido.
+
 6. ¿Como se comporta la función de valor de estado V(s)?
+    Es una funcion creciente, mientras mas inventario mas valor esperado tenemos.
+
 7. ¿Cómo cambiaría la política si la variabilidad de la demanda (lambda) aumenta de 4 a 8?
+    Simplemente pediriamos mas unidades en la mayoria de los casos para poder satisfacer la mayor demanda promedio.
 
 """
